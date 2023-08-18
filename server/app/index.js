6824210cb59d833ed0a5b9f3bd776f4c6b818e74
Romy <romane.pld@gmail.com>
2023-08-18 10:56:38 +0200

    add server

---
+++ server/app/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require('cors')
+
+const router = require('./routes')
+
+const app = express()
+app.use(cors())
+const port = 3001
+
+console.log('port', port)
+
+app.use(router)
+
+app.listen(port, () => console.log(`Magic happens on port ${port}`))
